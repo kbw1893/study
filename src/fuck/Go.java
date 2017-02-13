a35d73cf7c082e77ddaad320ef421a2d03a595c2
wanwon0727 <wanwonkbs@gmail.com>
2017-02-13 17:59:52 +0900

    도배도배 내가 다 도배할꺼야

--- src/fuck/Go.java
+++ src/fuck/Go.java
@@ -6,6 +6,7 @@ public class Go {
 		System.out.println("3ÇĞ³â 1¹İ Ä£±¸µé");
         System.out.println("Àú³á¹¹»çÁÙ²¨³Ä ¸ÛÃ»ÀÌ");
         System.out.println("¿ÏÂ¯¿ÏÂ¯ ");
+        System.out.println("ÀÌ ¶Ë¸ÛÃ»ÀÌ À¸ÈåÈåÈå µµ¹èµµ¹è");                         
 	}
 
 }
